0035fe6992138c519a662ecc50c2f2d94a3c903b
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-06-02 12:22:57 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,18 @@
 class FlatsController < ApplicationController
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :location, :type, :number_of_rooms, :price_per_month, :separation_allowed)
+  end
 end
