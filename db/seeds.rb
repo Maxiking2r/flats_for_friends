ccff1be6739e6960c30a7e22807aa77814cfcdb3
Frederike Soyka <f.m.soyka@googlemail.com>
2020-06-03 17:11:15 +0200

    Geocoding and Index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require "date"
+puts 'Cleaning the database'
+Booking.delete_all
+Flat.delete_all
+User.delete_all
+
+puts 'Creating fake user data...'
+
+User.create(
+  first_name: 'Peter',
+  last_name: 'Parker',
+  email: 'friendlyneighborhoodspider@web.com',
+  password: '123456'
+  )
+
+User.create(
+  first_name: 'Frederike',
+  last_name: 'Soyka',
+  email: 'frederikesoyka@web.com',
+  password: '123456'
+  )
+
+User.create(
+  first_name: 'Max',
+  last_name: 'Zweyer',
+  email: 'maxzweyer@web.com',
+  password: '123456'
+  )
+
+User.create(
+  first_name: 'Nain',
+  last_name: 'Mohd',
+  email: 'nainmohd@web.com',
+  password: '123456'
+  )
+
+flat1 =Flat.create(
+  name: 'Lovely flat in the center',
+  description: 'Centrally located beautifully furnished flat',
+  location: 'Berlin',
+  flat_type: 'flat',
+  number_of_rooms: 2,
+  price_per_month: 500,
+  separation_allowed: 1,
+  user: User.last,
+  )
+
+flat2 = Flat.create(
+  name: 'Spacious Kreuzberg room',
+  description: 'Large room located perfectly to get as much sunlight as possible',
+  location: 'Berlin',
+  flat_type: 'room',
+  number_of_rooms: 1,
+  price_per_month: 300,
+  separation_allowed: 2,
+  user: User.last,
+  )
+
+users = User.all
+
+Booking.create!(
+  start_date: Date.new(2020,7,1),
+  end_date: Date.new(2020,7,15),
+  user: users.sample,
+  flat: flat1
+  )
+
+Booking.create!(
+  start_date: Date.new(2020,8,1),
+  end_date: Date.new(2020,8,15),
+  user: users.sample,
+  flat: flat2
+  )
+
+Booking.create!(
+  start_date: Date.new(2020,9,1),
+  end_date: Date.new(2020,9,15),
+  user: users.sample,
+  flat: flat2
+  )
 
