1f864efacbd42d7939013a44c93d31cb49efc20f
Peter Hempel <hempel.pe@gmail.com>
2020-06-03 14:35:16 +0200

    add front-end flats#show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  # before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
     if params[:query].present?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  first_name: 'Peter',
+  last_name: 'Parker',
+  email: 'friendlyneighborhoodspider@web.com',
+  password: '123456'
+  )
+User.create(
+  first_name: 'Frederike',
+  last_name: 'Soyka',
+  email: 'frederikesoyka@web.com',
+  password: '123456'
+  )
+User.create(
+  first_name: 'Max',
+  last_name: 'Zweyer',
+  email: 'maxzweyer@web.com',
+  password: '123456'
+  )
+User.create(
+  first_name: 'Nain',
+  last_name: 'Mohd',
+  email: 'nainmohd@web.com',
+  password: '123456'
+  )
+Flat.create(
+  name: 'Lovely flat in the center',
+  description: 'Centrally located beautifully furnished flat',
+  location: 'Berlin',
+  flat_type: 'flat',
+  number_of_rooms: '2',
+  price_per_month: '500',
+  separation_allowed: '1',
+  user: User.last,
+  )
+Flat.create(
+  name: 'Spacious Kreuzberg room',
+  description: 'Large room located perfectly to get as much sunlight as possible',
+  location: 'Berlin',
+  flat_type: 'room',
+  number_of_rooms: '1',
+  price_per_month: '300',
+  separation_allowed: '2',
+  user: User.last,
+  )
+
+
+
