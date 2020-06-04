46728e545b105b6f2f9e2f56310288dc507d727c
Peter Hempel <hempel.pe@gmail.com>
2020-06-04 18:34:24 +0200

    front-end for bookings new and create and other front-end stuff plus adding model for messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :flats
   has_many :bookings
+  has_many :messages
   # validates :first_name, presence: true
   # validates :last_name, presence: true
 end
