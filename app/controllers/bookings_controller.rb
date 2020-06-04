07ad3c62a314cfd9e4ac4415b1050aa1459ba049
Frederike Soyka <f.m.soyka@googlemail.com>
2020-06-04 17:52:27 +0200

    Booking Index and Show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  before_action :set_flat, only: [:create, :new]
+
+  def new
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new
+  end
+
+  def index
+    @user = current_user
+    @bookings_as_renter = @user.bookings
+    @bookings_as_owner = Booking.joins(:flat).where(flats: { user_id: current_user.id })
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
 end
