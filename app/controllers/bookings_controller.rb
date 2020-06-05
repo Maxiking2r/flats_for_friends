b9d61e8e52edb86e3b5ee8da351431eb10361bd2
Peter Hempel <hempel.pe@gmail.com>
2020-06-05 11:23:00 +0200

    little bookingscontroller change

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,6 @@
 class BookingsController < ApplicationController
+  # before_action :set_flat, only: [:create, :new]
 
-  before_action :set_flat, only: [:create, :new]
-  
   def new
     @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
@@ -12,11 +11,11 @@ class BookingsController < ApplicationController
     @bookings_as_renter = @user.bookings
     @bookings_as_owner = Booking.joins(:flat).where(flats: { user_id: current_user.id })
   end
-  
+
   def show
     @booking = Booking.find(params[:id])
-  end 
-  
+  end
+
   def create
     @booking = Booking.new(booking_params)
     @flat = Flat.find(params[:flat_id])
