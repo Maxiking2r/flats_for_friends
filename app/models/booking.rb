f20d086e3b981134fde0d4bd84b411f7eb276da8
Maximilian Zweyer <63583932+Maxiking2r@users.noreply.github.com>
2020-06-05 14:31:07 +0200

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
-  validates :start_date, :end_date, presence: true, availability: true
+  validates :start_date, :end_date, presence: true
   # validates :end_date_after_start_date
   validates :confirmed, inclusion: { in: %w(declined pending confirmed) }
 
