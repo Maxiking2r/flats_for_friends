149dce9b6bd1e39f044e200a581aa3362909a600
Maximilian Zweyer <63583932+Maxiking2r@users.noreply.github.com>
2020-06-11 17:09:46 +0200

    Update simple_form_bootstrap.rb

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -118,7 +118,7 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label
     b.wrapper tag: 'div', class: 'd-flex flex-row justify-content-between align-items-center' do |ba|
-      ba.use :input, class: 'form-control mx-1', error_class: 'is-invalid'
+      ba.use :input, class: 'form-control mx-1', error_class: 'is-invalid', valid_class: 'is-valid'
     end
     b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
     b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
